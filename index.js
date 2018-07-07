388933bb3495d0af0d7f75bf2ebc9d0e2e77e59d
PaulaoDev <jskhanframework@gmail.com>
2018-07-07 19:05:28 -0300

    bot add

---
+++ index.js
@@ -0,0 +1,23 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '584173010:AAHvFZdLD47MweStNfGfJboI_TMCDTNw_WU';
+
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+
+});
+
+
+bot.on('message', (msg) => {
+
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Received your message ' + chatId);
+
+});
