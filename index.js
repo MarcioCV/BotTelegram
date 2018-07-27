ad085284255ac51e54e31a3234ee6926b97ba476
PaulaoDev <jskhanframework@gmail.com>
2018-07-27 15:45:11 -0300

    arrumado bug

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const fs = require('fs');
 const token = '661662510:AAEdhxymnv3H9sZisfyWpRoxSJ5jq2v666M';
 const telegram = new Telegraf(token);
 
+// telegram.on('text', (ctx) => {
+// 	console.log(ctx);
+// 	ctx.reply('Hello World')
+// 	return;
+// });
+
 require('./core')(telegram, db);
+
+telegram.catch((err) => {
+	console.error('\n---- ERROR telegram bot:\n');
+  	console.error(err);
+  	telegram.stop()
+});
 
 telegram.startPolling();   
